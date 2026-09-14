5c3bf31a419c6e83b2e7eea45cf881899a6b2511
BARBERBLAD\jeppe <Jeppefriislund@hotmail.dk>
2026-09-14 13:35:18 +0200

    Added MilkRecipeCalculator xUnit tests (closes #2)

---
+++ SuperChocolateMilk.Core/MilkRecipeCalculator.cs
@@ -0,0 +1,20 @@
+﻿namespace SuperChocolateMilk.core;
+
+public class MilkRecipeCalculator
+{
+    public static decimal CalculateChocolateSyrupRequired(decimal milkVolumeM1, string richnessLevel)
+    {
+        if (milkVolumeM1 <= 0) return 0m;
+
+        decimal baseSyrupM1 = milkVolumeM1 * 0.1m; //Base Syrup is 10% of milk volume
+
+        return richnessLevel.ToUpper() switch
+        {
+            "LIGHT" => baseSyrupM1 * 0.75m,
+            "MEDIUM" => baseSyrupM1,
+            "EXTRA" => baseSyrupM1 * 1.5m,
+            "ULTRA_COCO" => baseSyrupM1 * 2m,
+            _ => baseSyrupM1 // Default to medium if unknown richness level
+        };
+    }
+}
---
+++ SuperChocolateMilk.UnitTests/MilkRecipeCalculatorTests.cs
@@ -0,0 +1,18 @@
+﻿namespace SuperChocolateMilk.UnitTests;
+
+using Xunit;
+using SuperChocolateMilk.core;
+
+public class MilkRecipeCalculatorTests
+{
+    [Fact]
+    public void CalculateChocolateSyruprequired_RegularRichness_ReturnsTenPercentRatio()
+    {
+        int milkVolume = 1000;
+        String richness = "REGULAR";
+
+        decimal result = MilkRecipeCalculator.CalculateChocolateSyrupRequired(milkVolume, richness);
+        
+        Assert.Equal(100m, result);
+    }
+}
